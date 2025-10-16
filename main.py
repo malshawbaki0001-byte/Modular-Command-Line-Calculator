afceb6e06f8d2fe3778add423cf003e9b8b8afe5
malshawbaki0001-byte <malshawbaki0001@stu.kau.edu.sa>
2025-10-16 15:05:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,72 @@
-# import standard
-# import programmer
-# import scientific
-# import converter
+
 
 
 while True:
 
-    print("Welcome to Multi-Mode Calculator "
+    print("==== Welcome to Multi-Mode Calculator ===="
           "\n1. Standard Mode "
           "\n2. Programmer Mode "
           "\n3. Scientific Mode "
           "\n4. Converter Mode "
           "\n5. Exit ")
+    print("-"*42)
 
     model=eval(input("Enter your choice number: "))
 
+    print("-"*27)
+
 
     if model ==1:
-        # import standard
-
-        standard.main_menu()
 
         '''This mode handles basic arithmetic operations
-        , similar to a simple calculator.
-        '''
-    elif model ==2:
-        #import programmer
+         , similar to a simple calculator.
+                '''
+        import standard
+        
+        print("\n")
+        
+        standard.main_menu()
 
-        #programmer
+    elif model ==2:
 
         '''This mode focuses on operations useful for
-         programmers, including base conversions and 
-        bitwise operations.
-        '''
+                 programmers, including base conversions and 
+                bitwise operations.
+                '''
+        import programmer
+
+        print("\n")
+
+        programmer.main_menu()
+
     elif model ==3:
-        #import scientific
-
-        #scientific
-
+      
         '''This mode includes advanced mathematical 
-        functions, leveraging Python's math module.
-        '''
-    elif model ==4:
-        #import converter
+               functions, leveraging Python's math module.
+               '''
+        import scientific
+        
+        
+        scientific.main_menu()
+    
+        print("\n")
 
-        #converter
+    elif model ==4:
 
         '''This mode handles unit conversions
-         across categories.
-         '''
+                across categories.
+                '''
+        import converter
+       
+        print("\n")
+       
+        converter.main_menu()
+       
     elif model ==5:
+        print("\n\n\n\n\n")
 
+        print("-"*46,"\n| Thank you for using Multi-Mode Calculator  |\n"+"-"*46)
         break
 
     else:
-        print("invalid input")
-
-
-
+        print("Invalid input. Please enter a number between 1 and 5.")
